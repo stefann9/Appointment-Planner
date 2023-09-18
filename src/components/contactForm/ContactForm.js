c0cae70d9a563548f06a787c51320d209181d2e2
stefann9 <105439203+stefann9@users.noreply.github.com>
2023-09-18 14:20:28 +0300

    impplement ContactForm as stateless that renders form to collect contact info

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -1,16 +1,28 @@
 import React from "react";
 
-export const ContactForm = ({
-  name,
-  setName,
-  phone,
-  setPhone,
-  email,
-  setEmail,
-  handleSubmit
-}) => {
+export const ContactForm = ({ currentName, setCurrentName, phone, setPhone, email, setEmail, handleSubmit }) => {
   return (
-    <></>
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={currentName}
+        onChange={(e) => setCurrentName(e.target.value)}
+        placeholder="Name"
+      />
+      <input
+        type="tel"
+        value={phone}
+        onChange={(e) => setPhone(e.target.value)}
+        pattern="^[2-9]\d{2}-\d{3}-\d{4}$"
+        placeholder="Phone: 000-000-0000"
+      />
+      <input
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder="Email"
+      />
+      <button type="submit">Submit</button>
+    </form>
   );
 };
-
