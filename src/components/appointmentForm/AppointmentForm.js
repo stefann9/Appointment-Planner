51be5f5c9591825bc872114466071f0b3e15f5b8
stefann9 <105439203+stefann9@users.noreply.github.com>
2023-09-18 18:05:48 +0300

    implement AppointmentForm as stateless that renders form to get appointment info

--- src/components/appointmentForm/AppointmentForm.js
+++ src/components/appointmentForm/AppointmentForm.js
@@ -1,26 +1,34 @@
 import React from "react";
-
+import { ContactPicker } from "../contactPicker/ContactPicker";
 const getTodayString = () => {
-  const [month, day, year] = new Date()
-    .toLocaleDateString("en-US")
-    .split("/");
+  const [month, day, year] = new Date().toLocaleDateString("en-US").split("/");
   return `${year}-${month.padStart(2, "0")}-${day.padStart(2, "0")}`;
 };
 
 export const AppointmentForm = ({
   contacts,
-  title,
-  setTitle,
+  currentName,
+  setCurrentName,
   contact,
   setContact,
   date,
   setDate,
   time,
   setTime,
-  handleSubmit
+  handleSubmit,
 }) => {
-
   return (
-    <></>
+    <form onSubmit={handleSubmit}>
+      <input type="text" value={currentName} onChange={(e) => setCurrentName(e.target.value)} />
+      <input
+        type="date"
+        min={getTodayString()}
+        value={date}
+        onChange={(e) => setDate(e.target.value)}
+      />
+      <input type="time" value={time} onChange={(e) => setTime(e.target.value)} />
+      <ContactPicker name="contact" contacts={contacts} value={contact} onChange={(e) => setContact(e.target.value)} />
+      <button type="submit" >Submit</button>
+    </form>
   );
 };
