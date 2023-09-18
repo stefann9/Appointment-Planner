90609531d9274ed4e200898dd3b6e3ab1d9eeae5
stefann9 <105439203+stefann9@users.noreply.github.com>
2023-09-18 16:57:24 +0300

    implement TileList as stateless that renders a list of Tile comp

--- src/components/tileList/TileList.js
+++ src/components/tileList/TileList.js
@@ -1,9 +1,13 @@
 import React from "react";
+import { Tile } from "../tile/Tile";
 
-export const TileList = () => {
+export const TileList = ({ dataList }) => {
   return (
     <div>
-      
+      {dataList.map((data) => {
+        const { name, ...description } = data;
+        return <Tile key={name} name={name} description={description} />;
+      })}
     </div>
   );
 };
