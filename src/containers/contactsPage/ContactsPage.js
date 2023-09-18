89d245902099d6675fc93dac43941986d6465ed1
stefann9 <105439203+stefann9@users.noreply.github.com>
2023-09-18 16:55:54 +0300

    Change TileList prop name from contacts in dataList in ContactsPage, to be a generalized component

--- src/containers/contactsPage/ContactsPage.js
+++ src/containers/contactsPage/ContactsPage.js
@@ -41,7 +41,7 @@ export const ContactsPage = ({ contacts, handleAddContact }) => {
       <hr />
       <section>
         <h2>Contacts</h2>
-        <TileList contacts={contacts} />
+        <TileList dataList={contacts} />
       </section>
     </div>
   );
