405f5af2f90ca784e0facbb0718b3ba1e469b45a
stefann9 <105439203+stefann9@users.noreply.github.com>
2023-09-18 18:04:38 +0300

    implement AppointmentsPage as stateful that handles the logic for adding new appointments and listing them

--- src/containers/appointmentsPage/AppointmentsPage.js
+++ src/containers/appointmentsPage/AppointmentsPage.js
@@ -3,28 +3,43 @@ import React, { useState } from "react";
 import { AppointmentForm } from "../../components/appointmentForm/AppointmentForm";
 import { TileList } from "../../components/tileList/TileList";
 
-export const AppointmentsPage = () => {
-  /*
-  Define state variables for 
-  appointment info
-  */
+export const AppointmentsPage = ({ contacts, appointments, handleAddAppointment }) => {
+  const [currentName, setCurrentName] = useState("");
+  const [contact, setContact] = useState("");
+  const [date, setDate] = useState("");
+  const [time, setTime] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    /*
-    Add contact info and clear data  
-    */
-   
+
+    handleAddAppointment(currentName, contact, date, time);
+    setCurrentName("");
+    setContact("");
+    setDate("");
+    setTime("");
   };
 
   return (
     <div>
       <section>
         <h2>Add Appointment</h2>
+        <AppointmentForm
+          currentName={currentName}
+          setCurrentName={setCurrentName}
+          contact={contact}
+          setContact={setContact}
+          date={date}
+          setDate={setDate}
+          time={time}
+          setTime={setTime}
+          handleSubmit={handleSubmit}
+          contacts={contacts}
+        />
       </section>
       <hr />
       <section>
         <h2>Appointments</h2>
+        <TileList dataList={appointments} />
       </section>
     </div>
   );
