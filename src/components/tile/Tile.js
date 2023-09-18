2da725c19db085f1072c307c82de3b02dab25464
stefann9 <105439203+stefann9@users.noreply.github.com>
2023-09-18 16:58:19 +0300

    implement Tile as stateless that renders the data ffrom dataList prop

--- src/components/tile/Tile.js
+++ src/components/tile/Tile.js
@@ -1,9 +1,10 @@
 import React from "react";
 
-export const Tile = () => {
+export const Tile = ({name, description}) => {
   return (
     <div className="tile-container">
-      
+      <p className="tile-title" >{name}</p>
+      {Object.values(description).map((content,i) => <p className="tile" key={i} >{content}</p>)}
     </div>
   );
 };
