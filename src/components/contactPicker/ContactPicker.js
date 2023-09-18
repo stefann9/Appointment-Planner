fe3d0d712317968f6edd4b14c6de05f5c8c27ad4
stefann9 <105439203+stefann9@users.noreply.github.com>
2023-09-18 18:06:40 +0300

    implement ContactPicker as stateless that renders a drop-down list of all contact names

--- src/components/contactPicker/ContactPicker.js
+++ src/components/contactPicker/ContactPicker.js
@@ -1,7 +1,16 @@
 import React from "react";
 
-export const ContactPicker = () => {
+export const ContactPicker = ({ contacts, onChange, value, name }) => {
   return (
-    <></>
+    <select onChange={onChange} value={value} name={name}>
+      <option value="" default>
+        No Contact Selected
+      </option>
+      {contacts.map((contact) => (
+        <option key={contact.name} value={contact.name}>
+          {contact.name}
+        </option>
+      ))}
+    </select>
   );
 };
